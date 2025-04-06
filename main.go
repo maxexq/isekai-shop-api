a17655f0da717290d61bcf43ca73d267aaf65dd6
Parames Ajhan <parames.ajh@gmail.com>
2025-04-07 04:18:00 +0700

    feat: run start echo, add air hot reload

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"github.com/maxexq/isekei-shop-api/config"
+	"github.com/maxexq/isekei-shop-api/databases"
+	"github.com/maxexq/isekei-shop-api/server"
+)
+
+func main() {
+	conf := config.ConfigGetting()
+
+	db := databases.NewPosgresDatabase(conf.Database)
+
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
+
+}
