2feec059584a25bafe9c5f02421bcee543edc347
Parames Ajhan <parames.ajh@gmail.com>
2025-04-06 21:17:10 +0700

    fix: item entities

--- entities/item.go
+++ entities/item.go
@@ -9,7 +9,7 @@ type (
 		Name        string    `gorm:"type:varchar(64);unique;not null;"`
 		Description string    `gorm:"type:varchar(128);not null;"`
 		Picture     string    `gorm:"type:varchar(256);not null;"`
-		Price       uint      `gorm:"not null";`
+		Price       uint      `gorm:"not null;"`
 		IsAchive    bool      `gorm:"not null;default:false;"`
 		CreatedAt   time.Time `gorm:"not null;autoCreateTime;"`
 		UpdatedAt   time.Time `gorm:"not null;autoCreateTime;"`
