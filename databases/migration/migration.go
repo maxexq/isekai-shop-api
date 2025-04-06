fa598dee4d8c27f74775dd0300d6f879e9136975
Parames Ajhan <parames.ajh@gmail.com>
2025-04-07 01:50:02 +0700

    feat: setup migration

--- databases/migration/migration.go
+++ databases/migration/migration.go
@@ -1,3 +1,15 @@
-package migration
+package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/maxexq/isekei-shop-api/config"
+	"github.com/maxexq/isekei-shop-api/databases"
+)
+
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPosgresDatabase(conf.Database)
+
+	fmt.Println(db.ConnectionGetting())
+}
--- databases/postgresDatabase.go
+++ databases/postgresDatabase.go
@@ -1,3 +1,52 @@
 package databases
 
-type PostgresDatabase struct{}
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/maxexq/isekei-shop-api/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type postgresDatabase struct {
+	*gorm.DB
+}
+
+var (
+	postgresDatabaseInstance *postgresDatabase
+	once                     sync.Once
+)
+
+func NewPosgresDatabase(conf *config.Database) Database {
+	once.Do(func() {
+		dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s search_path=%s",
+			conf.Host,
+			conf.Port,
+			conf.User,
+			conf.Password,
+			conf.DBName,
+			conf.SSLMode,
+			conf.Schema,
+		)
+
+		conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("Connected to database %s", conf.DBName)
+
+		postgresDatabaseInstance = &postgresDatabase{
+			conn,
+		}
+	})
+
+	return postgresDatabaseInstance
+}
+
+func (db *postgresDatabase) ConnectionGetting() *gorm.DB {
+	return db.DB
+}
