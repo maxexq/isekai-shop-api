781450243fcb0cd148fe5d8c6b8ed07e0b0f26b9
Parames Ajhan <parames.ajh@gmail.com>
2025-04-07 02:17:59 +0700

    fix: entities, migration order

--- databases/migration/migration.go
+++ databases/migration/migration.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/maxexq/isekei-shop-api/config"
 	"github.com/maxexq/isekei-shop-api/databases"
+	"github.com/maxexq/isekei-shop-api/entities"
 )
 
 func main() {
 	conf := config.ConfigGetting()
 	db := databases.NewPosgresDatabase(conf.Database)
 
-	fmt.Println(db.ConnectionGetting())
+	playerMigration(db)
+	adminMigration(db)
+	itemMigration(db)
+	playerCoinMigration(db)
+	inventoryMigration(db)
+	purchaseHistoryMigration(db)
+
+}
+
+func playerMigration(db databases.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.Player{})
+}
+
+func adminMigration(db databases.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.Admin{})
+}
+
+func itemMigration(db databases.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.Item{})
+}
+
+func playerCoinMigration(db databases.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.PlayerCoin{})
+}
+
+func inventoryMigration(db databases.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.Inventory{})
+}
+
+func purchaseHistoryMigration(db databases.Database) {
+	db.ConnectionGetting().Migrator().CreateTable(&entities.PurchaseHistory{})
 }
